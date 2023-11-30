f76acc4666a1df91760a3a340adfd51376e1db5e
Janaki <zanuthulung@gmail.com>
2023-11-30 11:01:18 +0545

    initilize commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+const App = () => {
+  //bearer  is a type of access token used in HTTP requests that indicates that the request is authorized with a token
+
+  useEffect(() => {
+    const fetchAnimeData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:4000/api/anime`);
+        setanimeDate(response.data)
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchAnimeData()
+  }, [])
+  const [animeDate, setanimeDate] = useState([])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>Anime-app</h1>
+        <ul>
+          {animeDate.map(anime => (
+            <div className="list">
+              <p>hype:{anime.hype}</p>
+              <h4>{anime.studio}</h4>
+              <h3>{anime.title.text}</h3>
+              <p>{anime.description.substr(0, 200)}...</p>
+              <p>{anime.genres.join(',')}</p>
+              <p>start time:{anime.start_date}</p>
+              <a href={anime.title.link}>More Info</a>
+            </div>
+          ))}
+        </ul>
+
+      </div>
+    </>
   );
 }
 
---
+++ src/componenets/Animedetile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Animedetile=()=> {
+  return (
+      <div>
+          
+    </div>
+  )
+}
+
+export default Animedetile
